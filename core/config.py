485ff8559549a54d3c924cefbaf3145c1d2b7516
verages <87114157+verages@users.noreply.github.com>
2021-07-08 15:57:31 +0800

    Add files via upload

---
+++ core/config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Brief: 配置文件
+
+input_shape = (320, 320, 3)
+num_classes = 21
+
+epochs = 50
+batch_size = 4
+lr = 0.0001
+
+train_txt_path = "/Users/linzhihui/Desktop/Data/VOC2012/ImageSets/SegmentationAug/train.txt"
+val_txt_path = "/Users/linzhihui/Desktop/Data/VOC2012/ImageSets/SegmentationAug/val.txt"
+trainval_txt_path = "/Users/linzhihui/Desktop/Data/VOC2012/ImageSets/SegmentationAug/trainval.txt"
